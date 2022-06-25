fa75f08119fc7552b21354376329a0f93192dcd1
p0l1t1c1an <boicken19@gmail.com>
2022-06-24 23:06:53 -0500

    Working: serves my github.io pages

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+
+#[macro_use] extern crate rocket;
+
+use rocket::fs::{FileServer, relative};
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", FileServer::from(relative!("p0l1t1c1an.github.io")))
 }
+
