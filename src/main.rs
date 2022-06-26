349e7beb88d376a72e51598e5b8da81f55342656
p0l1t1c1an <boicken19@gmail.com>
2022-06-25 19:38:51 -0500

    Added Chroot

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 
-#[macro_use] extern crate rocket;
+use rocket::fs::{FileServer};
+use std::os::unix::fs::chroot;
+use std::env::set_current_dir;
 
-use rocket::fs::{FileServer, relative};
-
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", FileServer::from(relative!("p0l1t1c1an.github.io")))
+#[rocket::main]
+async fn main() -> anyhow::Result<()> {
+    chroot("/home/p0l1t1c1an/Programs/WebServer/p0l1t1c1an.github.io")?;
+    set_current_dir("/")?;
+    let _rocket = rocket::build()
+        .mount("/", FileServer::from("/"))
+        .launch().await?; 
+    Ok(())
 }
+
 
