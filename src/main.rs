521f4345b1828e606bf1378d1c59e6c0ccda0f8b
Jacob Boicken <boicken19@gmail.com>
2022-06-26 01:05:42 +0000

    Fixed user comment

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use privdrop::PrivDrop;
 async fn main() -> anyhow::Result<()> {
     PrivDrop::default()
     .chroot("/home/p0l1t1c1an/Programs/WebServer/p0l1t1c1an.github.io") // should be www directory
-    .user("p0l1t1c1an") // should be nobody in web
+    .user("p0l1t1c1an") // should be nobody
     .apply()?;
     let _ = rocket::build()
         .mount("/", FileServer::from("/"))
